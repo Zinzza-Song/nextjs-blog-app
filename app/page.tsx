5422c4513c54cf0072a2cbbbb80d2b85f1357502
Han-Bin Song <thdgksq@naver.com>
2025-04-01 13:42:22 +0900

    리스트 나열 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import homeStyles from './Home.module.css'
+import { getSortedPostsData } from '../lib/post'
 
 export default function Home() {
+  const allPostsData = getSortedPostsData()
+
   return (
     <div>
       <section className={homeStyles.headingMd}>
@@ -10,7 +13,17 @@ export default function Home() {
 
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
-        <ul className={homeStyles.list}></ul>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li
+              className={homeStyles.listItem}
+              key={id}>
+              <a>{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   )
