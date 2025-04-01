ba348c381b5995f9e61e8ffd4a5da0993ef9962b
Han-Bin Song <thdgksq@naver.com>
2025-04-01 15:49:14 +0900

    동적 메타데이터 생성 구현

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,12 +1,32 @@
 import { getPostData } from '@/lib/post'
 import homeStyles from '../../Home.module.css'
 import 'react'
+import { Metadata } from 'next'
 
+type Context = {
+  params: { id: string }
+}
 interface Post {
   date: string
   title: string
   id: string
   contentHtml: string
+}
+
+export async function generateMetadata({ params }: Context): Promise<Metadata> {
+  const { id } = await params
+  const postData = await getPostData(id)
+
+  return {
+    title: postData.title,
+    description: postData.title,
+    openGraph: {
+      title: postData.title,
+      description: postData.title,
+      type: 'website',
+      locale: 'ko_KR'
+    }
+  }
 }
 
 export default async function Post({
